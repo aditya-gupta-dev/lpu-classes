c69d5b2e0d0c98c80df5c90441f48825224255ed
Aditya Gupta <adityagupta2087@gmail.com>
2025-09-08 11:54:55 +0530

    new class details

---
+++ int108/8-9-25/main.py
@@ -0,0 +1,16 @@
+my_dict = { "key": 10, "some": 20 }
+print(my_dict["key"])
+
+
+f = 10 
+print(int(f))
+print(int("0xffaf", 16)) 
+binary_num = 0b0110101
+#print(int(binary_num, 2)) 
+
+print(float(203)) # 203.0 
+
+
+
+b =  0o725 
+print(b)
